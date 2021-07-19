e1ecdb95394b7eed11b99a32312ac0e072d533ca
Jens T. Rotter <jens.t.rotter@gmail.com>
2021-07-20 00:31:20 +0200

    Created JUCE Project
    JUCE Project Blank Plugin

---
+++ Source/PluginEditor.cpp
@@ -0,0 +1,40 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+//==============================================================================
+NEWJUCE0001AudioProcessorEditor::NEWJUCE0001AudioProcessorEditor (NEWJUCE0001AudioProcessor& p)
+    : AudioProcessorEditor (&p), audioProcessor (p)
+{
+    // Make sure that before the constructor has finished, you've set the
+    // editor's size to whatever you need it to be.
+    setSize (400, 300);
+}
+
+NEWJUCE0001AudioProcessorEditor::~NEWJUCE0001AudioProcessorEditor()
+{
+}
+
+//==============================================================================
+void NEWJUCE0001AudioProcessorEditor::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setColour (juce::Colours::white);
+    g.setFont (15.0f);
+    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+}
+
+void NEWJUCE0001AudioProcessorEditor::resized()
+{
+    // This is generally where you'll want to lay out the positions of any
+    // subcomponents in your editor..
+}
---
+++ Source/PluginEditor.h
@@ -0,0 +1,33 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class NEWJUCE0001AudioProcessorEditor  : public juce::AudioProcessorEditor
+{
+public:
+    NEWJUCE0001AudioProcessorEditor (NEWJUCE0001AudioProcessor&);
+    ~NEWJUCE0001AudioProcessorEditor() override;
+
+    //==============================================================================
+    void paint (juce::Graphics&) override;
+    void resized() override;
+
+private:
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    NEWJUCE0001AudioProcessor& audioProcessor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NEWJUCE0001AudioProcessorEditor)
+};
